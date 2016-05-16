963a72605cdc8ecb618f74e3160e970db3331c2c
renatn <zen@live.ru>
2016-05-16 17:22:03 +0300

    Added action add card test

---
+++ test/actions-test.js
@@ -0,0 +1,13 @@
+import { expect } from 'chai';
+import Actions from '../app/actions';
+
+describe('actions', () => {
+  it('should create an action to add a card', () => {
+    const expectedAction = {
+      type: 'ADD_CARD',
+      front: 'awesome',
+      back: 'классный'
+    }
+    expect(Actions.addCard('awesome', 'классный')).to.deep.equal(expectedAction);
+  })
+})
